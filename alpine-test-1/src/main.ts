809589a87fcc5c21bf8786a774c0e941bfd39a9b
Raymond Camden <raymondcamden@gmail.com>
2026-09-22 08:04:21 -0500

    misc

---
+++ alpine-test-1/src/main.ts
@@ -0,0 +1,22 @@
+import './style.css';
+import Alpine from 'alpinejs';
+
+type Cat = {
+  name: string;
+  breed: string;
+  gender: 'male' | 'female';
+};
+
+Alpine.data('app', () => ({
+  message:'Hello from Alpine!',
+  cats: [] as Cat[],
+  init() {
+    this.cats.push({ name: 'Whiskers', breed: 'Siamese', gender: 'male' });
+    this.cats.push({ name: 'Fluffy', breed: 'Persian', gender: 'female' });
+  },
+  meow(message:string = 'Meow!') {
+    alert(message);
+  }
+}));
+
+Alpine.start();
